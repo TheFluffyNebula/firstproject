ab6a51948f65b38655c337deccb2cd04d527dfb5
TheFluffyNeubla <jasonyin30@gmail.com>
2023-04-19 17:52:05 -0400

    dice: sum and products

--- src/main/java/first_maven_project/App.java
+++ src/main/java/first_maven_project/App.java
@@ -9,5 +9,22 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(root(69));
+        System.out.println(Math.pow(69,1.0/2));
+    }
+
+    public static double root(int x){
+        double L = 0;
+        double R = x;
+        while (R - L> Math.pow(10,-6)){
+            double mid = (L + R) / 2;
+            if (mid * mid < x){
+                L = mid;
+            }
+            else {
+                R = mid;
+            }
+        }
+        return (L + R) / 2;
     }
 }
